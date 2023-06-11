91064dca253daec59e94d3da81fa4525a8019f04
bykum1 <bykum1@hanmail.net>
2023-06-11 11:03:11 +0900

    클릭이 됨

--- src/main/java/com/forest/unongforest/gui/GuildGui.java
+++ src/main/java/com/forest/unongforest/gui/GuildGui.java
@@ -9,13 +9,9 @@ public class GuildGui {
 
     public static Inventory getInventory() {
         Inventory ret = Bukkit.createInventory(null, 3, "신호기");
-        for (int i = 0; i <= 26; i++) {
+        for (int i = 0; i <= 53; i++) {
             ret.setItem(i, new ItemStack(Material.GRAY_STAINED_GLASS_PANE));
         }
-        ret.setItem(10, new ItemStack(Material.PLAYER_HEAD));
-        ret.setItem(12, new ItemStack(Material.TOTEM_OF_UNDYING));
-        ret.setItem(14, new ItemStack(Material.EXPERIENCE_BOTTLE));
-        ret.setItem(16, new ItemStack(Material.ELYTRA));
         return ret;
     }
 }
