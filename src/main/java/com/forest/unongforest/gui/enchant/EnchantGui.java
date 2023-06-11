f9df30b2dcef31c0c761a6d319cddd7a079a448c
bykum1 <bykum1@hanmail.net>
2023-06-11 19:29:22 +0900

    인벤 이름 단축

--- src/main/java/com/forest/unongforest/gui/enchant/EnchantGui.java
+++ src/main/java/com/forest/unongforest/gui/enchant/EnchantGui.java
@@ -1,11 +1,18 @@
 package com.forest.unongforest.gui.enchant;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
 public class EnchantGui {
     public static Inventory getInventory() {
         Inventory EI = Bukkit.createInventory(null, 27, "마법부여");
+        for (int i = 0; i <= 44; i++) {
+            if (i != 22) {
+                EI.setItem(i, new ItemStack(Material.GRAY_STAINED_GLASS_PANE));
+            }
+        }
         return EI;
     }
 }
--- src/main/java/com/forest/unongforest/plugin/block/Enchant.java
+++ src/main/java/com/forest/unongforest/plugin/block/Enchant.java
@@ -7,7 +7,9 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.Inventory;
 
 public class Enchant implements Listener {
 
@@ -20,5 +22,11 @@ public class Enchant implements Listener {
                 p.openInventory(EnchantGui.getInventory());
             }
         }
+    }
+
+    @EventHandler
+    public void onInvClick(InventoryClickEvent e){
+        Inventory Inv = e.getClickedInventory();
+        Player p = (Player) e.getWhoClicked();
     }
 }
